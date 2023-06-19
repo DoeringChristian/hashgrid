3d16b230163d833c6ecef8ef392cdc4dc2cf8494
Döring Christian <christian.doering@um.de>
2023-06-19 18:59:08 +0200

    finally, working!

--- src/hashgrid/scatter_atomic.py
+++ src/hashgrid/scatter_atomic.py
@@ -9,7 +9,8 @@ from ._hashgrid_core import scatter_atomic_inc_uint_cuda
 
 def scatter_atomic_inc(target: dr.cuda.UInt, idx: dr.cuda.UInt):
     assert dr.is_cuda_v(target) and dr.is_cuda_v(idx)
-    assert isinstance(target, dr.cuda.UInt) and isinstance(idx, dr.cuda.UInt)
+    assert dr.is_unsigned_v(target) and dr.is_unsigned_v(idx)
+    assert dr.is_integral_v(target) and dr.is_integral_v(idx)
 
     n_values = dr.shape(idx)[-1]
     dst = dr.zeros(dr.cuda.UInt, n_values)  # type: dr.cuda.UInt
--- test.py
+++ test.py
@@ -1,12 +1,30 @@
 import drjit as dr
+import mitsuba as mi
 
-import hashgrid
+from hashgrid import HashGrid
 
-UInt = dr.cuda.UInt
+# UInt = dr.cuda.UInt
+#
+# target = UInt(0, 0, 0, 0)
+# idx = UInt(0, 0, 0, 0, 0, 0, 0)
+#
+# dst = hashgrid.scatter_atomic_inc(target, idx)
+# print(f"{dst=}")
+# print(f"{target=}")
 
-target = UInt(0, 0, 0, 0)
-idx = UInt(0, 0, 0, 0, 0, 0, 0)
+if __name__ == "__main__":
+    mi.set_variant("cuda_ad_rgb")
+    n = 20
+    sampler = mi.load_dict({"type": "independent"})  # type: mi.Sampler
+    sampler.seed(0, n)
+    sample = mi.Point3f(sampler.next_1d(), sampler.next_1d(), sampler.next_1d())
 
-dst = hashgrid.scatter_atomic_inc(target, idx)
-print(f"{dst=}")
-print(f"{target=}")
+    grid = HashGrid(sample, 3, 10)
+
+    p = mi.Point3f(0.5, 0.5, 0.1)
+    cell = grid.cell_idx(p)
+    cell_size = dr.gather(mi.UInt, grid.cell_size, cell)
+    offset = dr.gather(mi.UInt, grid.cell_offset, cell)
+    idx = dr.gather(mi.UInt, grid.sample_idx, offset)
+    x0 = dr.gather(mi.Point3f, sample, idx)
+    print(f"{x0=}")
